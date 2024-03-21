a24634d3182bb7790db581de815c501072bf1204
Karolina Tchiling <kdt@my.yorku.ca>
2024-03-21 12:21:14 -0400

    Update main

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import dataExtract as de
+import plots as plt
+
 
 
 def main():
-    de.extract('smartLoc_data/Berlin_PotsdamerPlatz/RXM-RAWX.csv')
+
+    #de.extract_raw('smartLoc_data/Berlin_PotsdamerPlatz/RXM-RAWX.csv')
+    plt.map_track()
 
 
 if __name__ == '__main__':
