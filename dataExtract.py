c6bc76f2827fb16ad5962c19f028fd2c0b22c46d
Karolina Tchiling <kdt@my.yorku.ca>
2024-03-21 12:20:37 -0400

    Addition of functions which extract columns in data and place into lists

--- dataExtract.py
+++ dataExtract.py
@@ -2,7 +2,8 @@ import csv
 import pandas as pd
 import numpy as np
 
-def extract(filepath):
+
+def extract_raw(filepath):
 
     with open(filepath, "r", newline="") as infile:
         reader = csv.reader(infile, delimiter=";")
@@ -11,10 +12,38 @@ def extract(filepath):
             writer = csv.writer(outfile, delimiter=",")
 
             for row in reader:
-                # row[0] = GPSweek, row[1] = GPSSeconds, row[28] = c/no, row[33] = NLOS information
-                extract_row = [row[0], row[1], row[28], row[33]]
+                # 0 = GPSweek, 1 = GPSSeconds,
+                # 2 = GT Longitude [deg], 4 = Latitude (GT Lat) [deg]
+                # 28  = c/no, 33 = NLOS information
+                extract_row = [row[0], row[1], row[2], row[4], row[28], row[33]]
                 writer.writerow(extract_row)
 
     print("\nExtraction completed. The selected columns have been saved to 'proccessed_data/PotsdamerPlatz-RAWX.csv'.\n")
+
+
+def extract_coordinates(filepath, col, type):
+
+    alist = []
+    with open(filepath, "r", newline="") as file:
+        reader = csv.reader(file, delimiter=",")
+        for row in reader:
+            alist.append(row[col])
+
+    alist = alist[1:]
+    alist = [type(item) for item in alist]
+    return alist
+
+
+def extract_NLOS(filepath, col):
+    alist = []
+    with open(filepath, "r", newline="") as file:
+        reader = csv.reader(file, delimiter=",")
+        for row in reader:
+            if row[col] == '0':
+                alist.append('LOS')
+            elif row[col] == '1':
+                alist.append('NLOS')
+    return alist
+
 
 
