dece77e1cdef24672c8f08de1a8ec4965bfc7e7a
Karolina Tchiling <kdt@my.yorku.ca>
2024-04-11 19:32:34 -0400

    Added cross validation

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import dataExtract as de
-import plots as plt
-import pandas as pd
-import models as md
+import plots as myplt
 import dataPreprocessing as dp
-from sklearn.preprocessing import LabelEncoder
+
+import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, cross_val_score, learning_curve, LearningCurveDisplay
 from sklearn.metrics import accuracy_score, classification_report
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import (StandardScaler, MinMaxScaler, RobustScaler,
-                                   MaxAbsScaler, QuantileTransformer, PowerTransformer)
 
 
-def main(transfored_df=None):
+def main():
 
-    # plt.map_track()
+    # display webmap of track
+    myplt.map_track()
 
     # DATA EXTRACTION -------------------------------------------------------------------------------------------------
 
@@ -45,9 +43,7 @@ def main(transfored_df=None):
 
     # DATA SPLITTING --------------------------------------------------------------------------------------------------
 
-    # split data: training set = 60% ; Validation set = 20% ; Testing set = 20%
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1)
 
     print("\nDATA SPLITS ---------------------------------")
     print("Total data points = " + str(len(df)))
@@ -55,26 +51,61 @@ def main(transfored_df=None):
     print("\nTraining Set - " + str(((len(y_train))/len(df))*100) + "%  (" + str(len(y_train)) + " data points)\n"
           + X_train.head().to_string()), print(y_train)
 
-    print("\nValidation Set - " + str(((len(y_val)) / len(df)) * 100) + "%  (" + str(len(y_val)) + " data points)\n"
-          + X_val.head().to_string()), print(y_val)
-
     print("\nTesting Set - " + str(((len(y_test)) / len(df)) * 100) + "%  (" + str(len(y_test)) + " data points)\n"
           + X_test.head().to_string()), print(y_test)
 
-    # ML TRAINING AND MODELING ---------------------------------------------------------------------------------------
+    # CROSS VALIDATION  ---------------------------------------------------------------------------------------
+
+    print("\nRFC Cross Validation ------------------------------------------\n")
+    rfc = RandomForestClassifier(n_estimators=100, random_state=42)                 # initialize the model
+    scores = cross_val_score(rfc, X_train, y_train, cv=5)                           # compute cross validation scores
+    print(scores)
+
+    print("%0.3f avg accuracy with a standard deviation of %0.3f" % (scores.mean(), scores.std()))
+
+    # plot the cross validation box plot
+    plt.boxplot(scores, patch_artist=True)
+    plt.title('Cross-Validation Score Distribution')
+    plt.ylabel('Accuracy')
+    plt.xticks([1], ['Random Forest'])
+    plt.show()
+
+    # learning curve output
+
+    # Accuracy learning curve
+    train_sizes, train_scores, validation_scores = learning_curve(rfc, X_train, y_train,
+                                                                  train_sizes=np.linspace(0.1, 1.0, 10),
+                                                                   cv=5, scoring='accuracy')
+    # Loss learning curve
+    # train_sizes, train_scores, validation_scores = learning_curve(rfc, X_train, y_train,
+    #                                                               train_sizes=np.linspace(0.1, 1.0, 10),
+    #                                                               cv=5, scoring='neg_log_loss')
+
+    # plot the learning curve
+    disp = LearningCurveDisplay(
+        train_sizes=train_sizes,
+        train_scores=train_scores,
+        test_scores=validation_scores,
+        score_name="accuracy"
+    )
+    disp.plot()
+    plt.show()
+
+    # ENTIRE ML TRAINING AND MODELING ---------------------------------------------------------------------------------
 
     # train random forest classifier
-    rfc = RandomForestClassifier(n_estimators=100, random_state=42)     # initialize the model
-    rfc.fit(X_train, y_train)                                           # train the model
-    y_pred = rfc.predict(X_test)                                        # run the model
+    rfc = RandomForestClassifier(n_estimators=100, random_state=42)  # initialize the model
+    rfc.fit(X_train, y_train)  # train the model
+    y_pred = rfc.predict(X_test)  # run the model
 
-    # # Accuracy
+    print("\nRFC Outputs ------------------------------------------\n")
+
+    # Accuracy
     print("Accuracy:", accuracy_score(y_test, y_pred))
-    #
-    # # Classification report
-    # print("Classification Report:")
-    # print(classification_report(y_test, y_pred))
 
+    # Classification report
+    print("\nClassification Report:")
+    print(classification_report(y_test, y_pred))
 
 
 if __name__ == '__main__':
