72f81b74446f2c1f3a1f3edc4177e04b2e68e07e
Karolina Tchiling <kdt@my.yorku.ca>
2024-05-09 12:15:56 -0400

    Addition of cross validation boxplot func and learning curve func

--- plots.py
+++ plots.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import train_test_split, cross_val_score, learning_curve, LearningCurveDisplay
 
 
 def threshold_plot(y_pred,y_test):
@@ -72,5 +73,43 @@ def false_negative_plot(y_pred, y_test):
     plt.ylabel('False Negative (%)')
     plt.grid(True)
     plt.show()
+
+
+def cross_val_plot(cross_val_scores, fold):
+    # plot the cross validation box plot
+    plt.figure(figsize=(8, 6))
+    plt.boxplot(cross_val_scores, patch_artist=True)
+    plt.title('Cross-Validation Score Distribution')
+    plt.ylabel('Accuracy')
+    plt.xticks([1], ['Random Forest'])
+
+    string = (f"Results based on {fold}-fold cross-validation\n "
+              f"{cross_val_scores.mean():.3f} avg accuracy with a standard deviation of {cross_val_scores.std():.3f}")
+
+    plt.subplots_adjust(bottom=0.15)
+    plt.figtext(0.5, 0.01, string,
+                ha="center", va="bottom", fontsize=10, color='black', fontweight='bold')
+
+    plt.show()
+
+
+def learning_curve_plot(model, X_train, y_train, fold, scoring, title):
+
+    # Accuracy learning curve
+    train_sizes, train_scores, validation_scores = learning_curve(model, X_train, y_train,
+                                                                  train_sizes=np.linspace(0.1, 1.0, 10),
+                                                                   cv=fold, scoring=scoring)
+    # plot the learning curve
+    disp = LearningCurveDisplay(
+        train_sizes=train_sizes,
+        train_scores=train_scores,
+        test_scores=validation_scores,
+        score_name=scoring
+    )
+    disp.plot()
+    plt.title(title)
+    plt.show()
+
+
 
 
